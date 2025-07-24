de7eaf8fca434b855e695c444a7cd753197297b3
rafirono13 <farhanmahbubrafi@gmail.com>
2025-07-24 06:32:41 +0600

    first TS practice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 let id: number = 5;
 let company: string = "TechCorp";
 let isPublished: boolean = true;
+
+// ----
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, "Hello", true];
+let xArr: any = "Hello World";
+// ----
+
+let concatenateValues = (a, b) => {
+  return a + b;
+};
+
+console.log(concatenateValues("Hello ", "World!"));
+console.log(concatenateValues(5, 10));
+console.log(concatenateValues(5, " apples"));
